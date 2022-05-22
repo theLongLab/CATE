429f77a647c57d645a8f400478308dde9a15efa1
Deshan Perera <55466094+DeshanPerera@users.noreply.github.com>
2022-05-21 20:26:28 -0600

    Update script_sample.R

--- Testing/Pop_Genome/R_script/script_sample.R
+++ Testing/Pop_Genome/R_script/script_sample.R
@@ -1,6 +1,6 @@
 library(vcfR)
 library(PopGenome)
 library(pegas)
-GENOME.class = readVCF("/work/long_lab/deshan/1000_Genome/Neutrality/testing/chromosomes_Raw/01.vcf.gz",10000,"1",11869,14409)
+GENOME.class = readVCF("01.vcf.gz",10000,"1",11869,14409)
 GENOME.class <- neutrality.stats(GENOME.class, FAST=TRUE)
 GENOME.class@Tajima.D
